97f2f97391513c4815d1f1c9ab3863f106d97522
Andriy Andryeyev <x0153362@gmail.com>
2018-10-13 22:18:20 +0300

    wip update grafana heartbeat

--- env/upd.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-data="cpu_load_short,host=server01,region=us-west value=$((1 + RANDOM % 100)) $(date +%s000000001)"
-echo $data
-curl -i -XPOST 'http://localhost:8186/write?db=qadb' --data-binary "$data"
